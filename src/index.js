875aeb2624db04c669e900ec92162a9b32e03fe5
Florian Gainza <florian.gainza.prof@gmail.com>
2020-05-16 21:15:51 -0500

    #72g2ab[closed] Create piano

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-class KeyButton extends React.Component {
+class Key extends React.Component {
   constructor(props) {
     super(props);
-
-    this.ws = new WebSocket('ws://localhost:8080/synt');
-
-    this.ws.onopen = function(evt) {
-      console.log('OPEN');
-    }
-
-    this.ws.onclose = function(evt) {
-      console.log('CLOSE');
-      this.ws = null;
-    }
-
-    this.ws.onmessage = function(evt) {
-      var note = new Audio();
-      var blob = new Blob([evt.data], { type: 'audio/wave' });
-      note.src = window.URL.createObjectURL(blob);
-      note.play();
-      console.log('RESPONSE: ' + evt.data);
-    }
-  
-    this.ws.onerror = function(evt) {
-      console.log('ERROR: ' + evt.data);
-    }
 
     this.state = { playing: false };
   }
@@ -38,15 +16,74 @@ class KeyButton extends React.Component {
 
     return (
       <button
-        onClick={() => this.ws.send('d#')}
+        onClick={this.props.onClick}
+        class={this.props.class + " piano-key"}
       >
-      C
+        {this.props.value}
       </button>
     );
   }
 }
 
+class Piano extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.ws = new WebSocket('ws://localhost:8080/synt');
+
+    this.ws.onopen = function (evt) {
+      console.log('OPEN');
+    }
+
+    this.ws.onclose = function (evt) {
+      console.log('CLOSE');
+      this.ws = null;
+    }
+
+    this.ws.onmessage = function (evt) {
+      var note = new Audio();
+      var blob = new Blob([evt.data], { type: 'audio/wave' });
+      note.src = window.URL.createObjectURL(blob);
+      note.play();
+      console.log('RESPONSE: ' + evt.data);
+    }
+
+    this.ws.onerror = function (evt) {
+      console.log('ERROR: ' + evt.data);
+    }
+  }
+
+  renderKey(pitch, isWhite) {
+    return (
+      <Key
+        value={pitch}
+        onClick={() => this.ws.send(pitch)}
+        class={isWhite ? 'wKey' : 'bKey'}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div class="piano">
+        {this.renderKey('c', true)}
+        {this.renderKey('c#', false)}
+        {this.renderKey('d', true)}
+        {this.renderKey('d#', false)}
+        {this.renderKey('e', true)}
+        {this.renderKey('f', true)}
+        {this.renderKey('f#', false)}
+        {this.renderKey('g', true)}
+        {this.renderKey('g#', false)}
+        {this.renderKey('a', true)}
+        {this.renderKey('a#', false)}
+        {this.renderKey('b', true)}
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <KeyButton />,
+  <Piano />,
   document.getElementById('keyboard_container')
 );
