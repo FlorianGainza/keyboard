ff6aad33fca9198cc9e86b71304170aa8c92dd6d
Florian Gainza <florian.gainza.prof@gmail.com>
2020-05-16 20:17:15 -0500

    #72a1d4 Update front environment

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
-'use strict';
-
-const e = React.createElement;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class KeyButton extends React.Component {
   constructor(props) {
     super(props);
+
     this.ws = new WebSocket('ws://localhost:8080/synt');
+
     this.ws.onopen = function(evt) {
       console.log('OPEN');
     }
+
     this.ws.onclose = function(evt) {
       console.log('CLOSE');
       this.ws = null;
     }
+
     this.ws.onmessage = function(evt) {
       var note = new Audio();
       var blob = new Blob([evt.data], { type: 'audio/wave' });
-      var url = window.URL.createObjectURL(blob);
-      note.src = url
+      note.src = window.URL.createObjectURL(blob);
       note.play();
       console.log('RESPONSE: ' + evt.data);
     }
+  
     this.ws.onerror = function(evt) {
       console.log('ERROR: ' + evt.data);
     }
+
     this.state = { playing: false };
   }
 
@@ -32,13 +36,17 @@ class KeyButton extends React.Component {
       return 'You are playing note';
     }
 
-    return e(
-      'button',
-      { onClick: () => this.ws.send('C') },
-      'C'
+    return (
+      <button
+        onClick={() => this.ws.send('d#')}
+      >
+      C
+      </button>
     );
   }
 }
 
-const domContainer = document.querySelector('#keyboard_container');
-ReactDOM.render(e(KeyButton), domContainer);
+ReactDOM.render(
+  <KeyButton />,
+  document.getElementById('keyboard_container')
+);
